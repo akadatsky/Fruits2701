126223e5903cf1c361c226b29463e24a6617c295
Galina Chitak <gala199@list.ru>
2022-01-30 15:49:38 +0300

    Add main1

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -1,5 +1,8 @@
 package com.galinachitakV.Fruits2701.src.com.akadatsky;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     /*
@@ -23,5 +26,66 @@ public class Main {
         cashMachine.add(GreenApple);
         cashMachine.printBill();
 
+        main1();
+
     }
+    public static void main1() {
+        /*Реализовать метод который сортирует массив целых чисел быстрой сортировкой.*/
+        Random random = new Random();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+        }
+
+        System.out.println("Было");
+        System.out.println(Arrays.toString(arr));
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        quickSort(arr, left, right);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] array, int left, int right) {
+        if (array.length == 0)
+            return;
+
+        if (left >= right)
+            return;
+
+        int middle = left + (right - left) / 2;
+        int count = array[middle];
+
+        int i = left, j = right;
+        while (i <= j) {
+            while (array[i] < count) {
+                i++;
+            }
+
+            while (array[j] > count) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (left < j)
+            quickSort(array, left, j);
+
+        if (right > i)
+            quickSort(array, i, right);
+    }
+
 }
+
+
+
+
