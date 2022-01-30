1bdf0361a8510ca05a2e4905145e8b72cedc905e
Galina Chitak <gala199@list.ru>
2022-01-30 14:05:21 +0300

    Update main

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -11,11 +11,16 @@ public class Main {
     public static void main(String[] args) {
 
         Fruit lemon = new Fruit(Fruit.FruitType.LEMON, Fruit.Color.YELLOW, 24.90, 198);
-        Fruit banana = new Fruit(Fruit.FruitType.LEMON, Fruit.Color.YELLOW, 24.90, 190);
+        Fruit banana = new Fruit(Fruit.FruitType.BANANA, Fruit.Color.YELLOW, 24.90, 190);
+        Fruit RedApple = new Fruit(Fruit.FruitType.APPLE, Fruit.Color.RED, 14.90, 228);
+        Fruit GreenApple = new Fruit(Fruit.FruitType.APPLE, Fruit.Color.GREEN, 24.90, 232);
+
 
         CashMachine cashMachine = new CashMachine();
         cashMachine.add(lemon);
         cashMachine.add(banana);
+        cashMachine.add(RedApple);
+        cashMachine.add(GreenApple);
         cashMachine.printBill();
 
     }
